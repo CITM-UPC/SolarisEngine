b98315cbb2da267d701a4aa92093ad4a1bd77b14
Marti Sabaté <sabatemiranda@gmail.com>
2024-09-25 12:14:48 +0200

    test [posiblemente haya que borrar]

---
+++ SolarisEngine/SolarisEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SolarisEngine.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "SolarisEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
