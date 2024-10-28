e63d74861f29005d6386ffae693eda67d70eee3f
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2024-10-28 19:04:33 +0100

    MSM - MenuBar añadidos
    Link de github
    Opciones al estilo unity

--- SolarisEngine/sdl2_simple_example/sdl2_simple_example/MenuBar.cpp
+++ SolarisEngine/sdl2_simple_example/sdl2_simple_example/MenuBar.cpp
@@ -64,11 +64,14 @@ void MenuBar::Render() {
     }
 
     if (ImGui::BeginMainMenuBar()) {
-        if (ImGui::BeginMenu("Menu")) {
+        if (ImGui::BeginMenu("General")) {
             if (ImGui::MenuItem("Exit")) {
                 SDL_Event quit_event;
                 quit_event.type = SDL_QUIT;
                 SDL_PushEvent(&quit_event);
+            }
+            if (ImGui::MenuItem("About")) {
+                SDL_OpenURL("https://github.com/Solaris-Group/SolarisEngine");
             }
             if (ImGui::MenuItem("ShowDemo")) {
                 showDemo = !showDemo;
@@ -91,45 +94,70 @@ void MenuBar::Render() {
             ImGui::EndMenu();
         }
 
-        if (ImGui::BeginMenu("Primitive")) {
-            if (ImGui::MenuItem("Plane")) {
-                auto newGO = GameObject::Create("Plane");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GeneratePlaneMesh();
-                app->gameObjects.push_back(newGO);  // Agregar a la lista global de GameObjects
-            }
-            if (ImGui::MenuItem("Cube")) {
-                auto newGO = GameObject::Create("Cube");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GenerateCubeMesh();
-                app->gameObjects.push_back(newGO);
-            }
-            if (ImGui::MenuItem("Triangle")) {
-                auto newGO = GameObject::Create("Triangle");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GenerateTriangleMesh();
-                app->gameObjects.push_back(newGO);
-            }
-            if (ImGui::MenuItem("Sphere")) {
-                auto newGO = GameObject::Create("Sphere");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GenerateSphereMesh();
-                app->gameObjects.push_back(newGO);
-            }
-            if (ImGui::MenuItem("Capsule")) {
-                auto newGO = GameObject::Create("Capsule");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GenerateCapsuleMesh();
-                app->gameObjects.push_back(newGO);
-            }
-            if (ImGui::MenuItem("Cylinder")) {
-                auto newGO = GameObject::Create("Cylinder");
-                auto mesh = newGO->AddComponent<Component_Mesh>();
-                mesh->GenerateCylinderMesh();
-                app->gameObjects.push_back(newGO);
-            }
+        
+
+        if (ImGui::BeginMenu("General")) {
             ImGui::EndMenu();
         }
+        if (ImGui::BeginMenu("Edit")) {
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Assets")) {
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("GameObject")) {
+
+            if (ImGui::BeginMenu("Primitive")) {
+                if (ImGui::MenuItem("Plane")) {
+                    auto newGO = GameObject::Create("Plane");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GeneratePlaneMesh();
+                    app->gameObjects.push_back(newGO);  // Agregar a la lista global de GameObjects
+                }
+                if (ImGui::MenuItem("Cube")) {
+                    auto newGO = GameObject::Create("Cube");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GenerateCubeMesh();
+                    app->gameObjects.push_back(newGO);
+                }
+                if (ImGui::MenuItem("Triangle")) {
+                    auto newGO = GameObject::Create("Triangle");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GenerateTriangleMesh();
+                    app->gameObjects.push_back(newGO);
+                }
+                if (ImGui::MenuItem("Sphere")) {
+                    auto newGO = GameObject::Create("Sphere");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GenerateSphereMesh();
+                    app->gameObjects.push_back(newGO);
+                }
+                if (ImGui::MenuItem("Capsule")) {
+                    auto newGO = GameObject::Create("Capsule");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GenerateCapsuleMesh();
+                    app->gameObjects.push_back(newGO);
+                }
+                if (ImGui::MenuItem("Cylinder")) {
+                    auto newGO = GameObject::Create("Cylinder");
+                    auto mesh = newGO->AddComponent<Component_Mesh>();
+                    mesh->GenerateCylinderMesh();
+                    app->gameObjects.push_back(newGO);
+                }
+                ImGui::EndMenu();
+            }
+
+
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Component")) {
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Window")) {
+            ImGui::EndMenu();
+        }
+
+       
 
         ImGui::EndMainMenuBar();
     }
