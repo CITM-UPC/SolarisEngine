b1dabfd1876272646a24efc04814d38d24b7d4ff
Marti Sabaté <sabatemiranda@gmail.com>
2024-11-13 10:19:16 +0100

    MSM - Grid en las escenas

--- SolarisEngine/SolarisEngine/SolarisEngine/Scene.cpp
+++ SolarisEngine/SolarisEngine/SolarisEngine/Scene.cpp
@@ -1,4 +1,5 @@
 #include "Scene.h"
+#include "App.h"
 
 Scene::Scene()
 {
@@ -6,6 +7,9 @@ Scene::Scene()
 
 void Scene::Update(double dt)
 {
+
+    DrawInfiniteGrid(1, 50);
+
     for (GameObject* gameObject : gameObjects)
     {
         if (gameObject && gameObject->IsEnabled()) {
@@ -65,3 +69,67 @@ GameObject* Scene::GetCopiedGameObject()
 {
     return this->copiedGameObject;
 }
+
+void Scene::DrawGrid(float size = 10.0f, int divisions = 10)
+{
+    // Define el tamaño de cada celda en el grid
+    float step = size / divisions;
+    float halfSize = size / 2.0f;
+
+    glPushMatrix();
+    glBegin(GL_LINES);
+
+    // Define el color del grid (por ejemplo, gris claro)
+    glColor3f(0.5f, 0.5f, 0.5f);
+
+    // Dibujar líneas en el eje X
+    for (int i = 0; i <= divisions; ++i) {
+        float pos = -halfSize + i * step;
+
+        // Línea paralela al eje Z
+        glVertex3f(pos, 0, -halfSize);
+        glVertex3f(pos, 0, halfSize);
+
+        // Línea paralela al eje X
+        glVertex3f(-halfSize, 0, pos);
+        glVertex3f(halfSize, 0, pos);
+    }
+
+    glEnd();
+    glPopMatrix();
+}
+
+
+void Scene::DrawInfiniteGrid(float gridSpacing = 1.0f, int halfGridLines = 50)
+{
+
+    vec3 cameraPosition = app->cameraEditor->GetCameraPosition();
+
+    // Calcula la posición del grid basado en la cámara para centrarlo
+    float gridOffsetX = fmod(cameraPosition.x, gridSpacing);
+    float gridOffsetZ = fmod(cameraPosition.z, gridSpacing);
+
+    glPushMatrix();
+    glBegin(GL_LINES);
+
+    // Color del grid (gris claro)
+    glColor3f(0.7f, 0.7f, 0.7f);
+
+    // Dibujar líneas paralelas al eje Z
+    for (int i = -halfGridLines; i <= halfGridLines; ++i) {
+        float x = i * gridSpacing - gridOffsetX;
+        glVertex3f(x, 0.0f, -halfGridLines * gridSpacing - gridOffsetZ);
+        glVertex3f(x, 0.0f, halfGridLines * gridSpacing - gridOffsetZ);
+    }
+
+    // Dibujar líneas paralelas al eje X
+    for (int j = -halfGridLines; j <= halfGridLines; ++j) {
+        float z = j * gridSpacing - gridOffsetZ;
+        glVertex3f(-halfGridLines * gridSpacing - gridOffsetX, 0.0f, z);
+        glVertex3f(halfGridLines * gridSpacing - gridOffsetX, 0.0f, z);
+    }
+
+    glEnd();
+    glPopMatrix();
+}
+
